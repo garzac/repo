344ca585e75aa5847b70302748adf1938613afe7
garzac <54555804+garzac@users.noreply.github.com>
2026-08-12 16:52:44 -0500

    Update keras3_model_classification_2.R

--- keras_model_classification_2.R
+++ keras_model_classification_2.R
@@ -1,182 +1,145 @@
 
-# The approach of this model is very similar to keras_model_classification.R 
-# The main difference is how I apply the Feature_Engineering on the data 
-# (one-hot-enconding, normalization, etc) is done with the library tfdataset 
-# that is based on Tensorflow-Dataset API. The model will learn about 15 classes  
-# that are based on the price and this time the classes are integers.
-library(keras)
-library(tensorflow)
-library(tfdatasets)
+# The following model is using the libraries Keras3 and Tensorflow and the data
+# used is the diamonds dataset. Instead of doing a regression-model, I turn it 
+# into a classification-model. Where I break down the variable price and convert 
+# it into factors (classes). The model will learn about 15 classes  that are 
+# based on the variable price. And the model it will learn the class based on 
+# the variables of the diamonds dataset.
+
+# Loading the libraries
+library(keras3)
 library(tidyverse)
+library(tensorflow, exclude = c("shape", "set_random_seed"))
+library(tfdatasets, exclude = 'shape')
 
-# Now reset the  keras  session & set up the random numbers from tensorflow 
-tf$keras$backend$clear_session()
-tf$keras$utils$set_random_seed(51L)
+# Reset the keras session
+clear_session(free_memory = TRUE)
 
-# Select the diamonds dataset and then remove the duplicates 
+# Now set up the random number for all the environment
+set_random_seed(51L)
+
+# Remove the duplicates and store it in the data-object
 data <- distinct(diamonds)
 
-# Now apply transformations on the dataset. First relocate the variable carat
-# in the last. Second the data-type of the categorical variables cut, color and
-# clarity are factors. Tensorflow doesn't recognize this data-type. So, 
-# I need to coerce this factors to strings. Third break down the variable price 
-# and convert it into classes. Each class is in an interval of 1200. And it will
-# be 15 classes. Class 1 is from minimum value $326 to the value $1200.
-# Class 2 is from value $1201 to the value $2400 and so on, until
-# Class 15 is from value $16801 to the max value $18823. And finally convert the
-# data type of the variable-class  to integer
-data <- data |> relocate(-carat) |>  mutate(cut = as.character(cut), 
-    color = as.character(color), clarity = as.character(clarity), 
-    class = cut(price, breaks = c(0, 1200, 2400, 3600, 4800, 6000, 7200, 
-    8400, 9600, 10800, 12000, 13200, 14400, 15600, 16800, 19000), 
-    labels = c(1:15), include.lowest = TRUE)) |> 
-    mutate(class = as.integer(class)) 
+# Now, I'll check the price variable and break down into factors (classes).
+# For this I will use mutate() to create a new variable called class. And within
+# mutate, I'll use cut() that will break down the variable price into factors.
+# Each class is in an interval of 1200. And it will be 15 classes.
+# Class 0 is from minimum value $326 to the value $1200.
+# Class 1 is from value $1201 to the value $2400 and so on, until
+# Class 14 is from value $16801 to the max value $18823.
+# Also can check the minimum, maximum, quantity and distinct prices by each class.
+# Besides based of the metrics, I notice that all the classes are not balanced.
+# And based on that, during training I'll use the appropriate metrics
+data |> mutate(class = cut(price, breaks = c(0, 1200, 2400, 3600, 4800, 6000, 
+        7200, 8400, 9600, 10800, 12000, 13200, 14400, 15600, 16800, 19000),
+        labels = c(0:14), include.lowest = TRUE)) |> group_by(class) |> 
+    summarise(min_price = min(price), max_price = max(price), quantity = n(), 
+              distinct_price = n_distinct(price))
 
-# Now, I'm going to create a vector of indexes that later I'll be using for 
-# making predictions. The size of the dataframe is 3000 rows. 
-set.seed(51)
-index_to_predict <- sample(x = nrow(data), size = 3000, replace = FALSE)
+# Now, I'm going to inspect the correlation between all the numeric variables with
+# respect to the variable price. And the metric shows that the correlation of the 
+# variables depth and table are near to zero. So, both variables it'll discard.
+cor(data[, -c(2:4)])
 
-# Now, I'm going to use the vector indexes to extract the rows from the dataset 
-# and store it in the dataset to_predict
-to_predict <- data[index_to_predict, ]
-# Now, eliminate all the row-numbers in the indexes from the dataset data.
-data <- data[-index_to_predict, ]
-# Verify that none row of the dataset data exists in the dataset to_predict
-intersect(data, to_predict)
+# Now, is time for transform the dataset. First I'm going to relocate the variables
+# carat and price as the last variables. Then I'm going to reuse the code to create 
+# the variable class and then covert it to an integer-type. Third I'm going to 
+# eliminate the variables table, depth and price. Then I'm going to convert the
+# categorical variables cut, color and clarity to character-type. And finally
+# I will convert the variable class as one-hot-encode
+data <- data |> relocate(carat, price, .after = last_col()) |>
+    mutate(class = cut(price, breaks = c(0, 1200, 2400, 3600, 4800, 6000, 7200, 
+        8400, 9600, 10800, 12000, 13200, 14400, 15600, 16800, 19000), 
+        labels = c(0:14), include.lowest = TRUE)) |> 
+    mutate(class = as.integer(class)) |> select(-c(table, depth, price)) |> 
+    mutate(cut = as.character(cut), color = as.character(color), 
+        clarity = as.character(clarity)) |> mutate(class = to_categorical(class))
 
-# Now that the variable-class will be the variable-Y, I'm going to eliminate
-# the variable price in the dataset
-data <- data |> select(-price)
 
 # Now I create a vector of indices that I'll use to split the dataframe
 set.seed(51)
-index <- sample(x = 1:3, size = nrow(data), replace = TRUE, prob = c(.8, .1, .1))
+index <- sample(x = 3, size = nrow(data), replace = TRUE, prob = c(.7, .15, .15))
+
+# Check the length for each index
+table(index)
 
 # Check the proportions for each index
 prop.table(table(index))
 
-# Now I'll split the dataset, I use the index vector to split the dataset.
+# Now I'll split the dataset, I use the index (vectors) to split the dataset.
 # Index 1 is for train set, index 2 for validation set, index 3 for test set
-train <- data[index == 1, ]
-val <- data[index == 2, ]
-test <- data[index == 3, ]
+train_data <- data[index == 1, ]
+val_data <- data[index == 2, ]
+test_data <- data[index == 3, ]
 
-# I apply a Feature-Engineering on the data, using the Tensorflow-Dataset API. 
-# First create a feature_spec that will contain the transformations and on this 
-# step I define the variable-class as the Y (label) and the rest of variables as 
-# the X. Second convert the categorical variables cut, color and clarity to 
-# one-hot-encoding. Third define the rest of the variables as numeric-columns 
-# and then apply a scaler_standar() to normalize the variables. 
-spec <- feature_spec(data, class ~ .)
-spec <- spec |> step_categorical_column_with_vocabulary_list(cut, 
-    vocabulary_list = c('Fair', 'Good', 'Very Good', 'Premium', 'Ideal'), 
-    num_oov_buckets = 0L) |> step_indicator_column(cut)
-spec <- spec |> step_categorical_column_with_vocabulary_list(color, 
-    vocabulary_list = c("D", "E", "F", "G", "H", "I", "J"), 
-    num_oov_buckets = 0L) |> step_indicator_column(color)
-spec <- spec |> step_categorical_column_with_vocabulary_list(clarity, 
-    vocabulary_list = c("I1",   "SI2",  "SI1",  "VS2",  "VS1",  "VVS2", "VVS1", 
-    "IF"), num_oov_buckets = 0L) |> step_indicator_column(clarity)
-spec <- spec |> step_numeric_column(depth, normalizer_fn = scaler_standard())
-spec <- spec |> step_numeric_column(table, normalizer_fn = scaler_standard())
-spec <- spec |> step_numeric_column(x, normalizer_fn = scaler_standard())
-spec <- spec |> step_numeric_column(y, normalizer_fn = scaler_standard())
-spec <- spec |> step_numeric_column(z, normalizer_fn = scaler_standard())
-spec <- spec |> step_numeric_column(carat, normalizer_fn = scaler_standard())
+# It is time to apply some Feature-Engineering on the dataset, using the 
+# Tensorflow-Dataset API. First create a feature_space that will contain all the 
+# transformations. Second convert the categorical variables cut, color and clarity 
+# variables to one-hot-encoding. Third define the rest of the variables as 
+# numeric-columns and then in this step also normalize the variables.
+# Next, I fit the feature_space using the function adapt() 
+feature_space <- layer_feature_space(features = list(
+    cut = feature_string_categorical(num_oov_indices = 0, output_mode = 'one_hot'),
+    color = feature_string_categorical(num_oov_indices = 0, output_mode = 'one_hot'),
+    clarity = feature_string_categorical(num_oov_indices = 0, output_mode = 'one_hot'),
+    x = feature_float_normalized(),
+    y = feature_float_normalized(),
+    z = feature_float_normalized(),
+    carat = feature_float_normalized()))
 
-# To see the steps in the feature_spec
-spec$steps
+feature_space |> adapt(train_data |> select(-class))
 
-# Now fit() the feature_spec
-spec$fit()
 
-# To see the list of dense-features in the spec
-str(spec$dense_features())
+# Then I'm going to extract and the create the respective inputs, features and 
+# outputs from feature_space to build a new model
+inputs <- feature_space$get_inputs()
 
-# Next, I'm going to build two distinct callbacks that it'll be used during the
-# training. The first callback will monitor the loss in validation-set and when the
-# loss stops decreasing, the callback it will change the learning rate. The other
-# callback also will monitor the accuracy in the validation-set and when the 
-# accuracy stops increasing the callback it will stop the training and also it'll 
-# RESTORE THE BEST WEIGHTS in the training
-lr <- callback_reduce_lr_on_plateau(monitor = 'val_loss',
-        patience = 15, verbose = 1, mode = 'min', min_lr = .0001)
+features <- feature_space$get_encoded_features()
 
-stoping <- callback_early_stopping(monitor = 'val_accuracy', patience = 35, 
-                verbose = 1, mode ='max', restore_best_weights = TRUE)
-
-# Now, its time to build the model. For this I'm going to use the Functional-API
-# Define the inputs of the model and store it in inputs
-inputs <- layer_input_from_dataset(train |>  select(-class))
-
-# Then define the rest of the model and store it in outputs
-outputs <- inputs %>% layer_dense_features(dense_features(spec)) |> 
-    layer_dense(units = 128, activation = 'relu') |> 
-    layer_dense(units = 64, activation = 'relu') |> 
+outputs <- features |> layer_dense(units = 512, activation = 'relu') |> 
+    layer_dense(units = 256, activation = 'relu') |> 
     layer_dense(units = 16, activation = 'softmax')
 
-
-# Now define the model with the inputs and outputs
-model <- keras_model(inputs = inputs, outputs = outputs)
+# Now build the model using the respectives inputs and outputs
+model <-  keras_model(inputs =  inputs, outputs =  outputs)
 
 # Inspect the model's architecture
 summary(model)
 
-# Now I compile the model with its optimizer, loss and metrics.
+# Now I compile the model with its optimizer, loss and metrics. Because the clases
+# of the dataset are unbalanced, for the metrics argument I'll use the metrics
+# predicion and recall
 model |> compile(optimizer = optimizer_adam(), 
-                 loss = loss_sparse_categorical_crossentropy(), 
-                 metrics = 'accuracy')
+                 loss = loss_categorical_crossentropy(), 
+                 metrics = c('precision', 'recall'))
 
-# Then is time for training. For the X, I'll use the dataset and  eliminate the
-# variable-class. For the Y define the variable-class as a vector. It applies
-# the same approach with the parameter validation_data with their respective
-# validation dataset. Also I'm going to use both callbacks in the callback 
-# parameter. The training stop in the epoch 96 and I get the following metrics:
-# For train dataset: loss = .3734; accuracy  = .8532
-# For validation dataset: loss = .4433; accuracy = .8226
-history <- model |> fit(x = train |> select(-class), y = train$class, 
-    batch_size = 64, epochs = 100, verbose = 1, 
-    validation_data = list(val |>  select(-class), val$class),
-    callbacks = list(lr, stoping))
+# Next, I'm going to build two distinct callbacks that it'll be used during the
+# training. The first callback will monitor the LOSS in validation-set and when 
+# LOSS stops decreasing, the callback it will change the learning rate. The other
+# callback also will monitor the PRECISION in the validation-set and when the 
+# PRECISION stops increasing the callback it will stop the training and also 
+# it'll RESTORE THE BEST WEIGHTS in the training
+lr <- callback_reduce_lr_on_plateau(monitor = 'val_loss',
+        patience = 40, verbose = 1, mode = 'min', min_lr = .0001)
 
-# Inspect the metrics
-plot(history)
+stopping <- callback_early_stopping(monitor = 'val_precision', patience = 60, 
+        verbose = 1, mode ='max', restore_best_weights = TRUE)
 
-# Now evaluate the model on test dataset. And applies the same approach as 
-# in training. I get: loss = .5635; accuracy = .8099
-model |> evaluate(test |>  select(-class), test$class)
+# Now, it's time for training. I'll use the respective data and the callbacks. 
+# Training is for 100 epochs, but the training stops in the epoch 61 and the callback
+# retrieves the weights from epoch 1. The metrics obtained from training are:
+# For train dataset: loss = .8007; precision = .8726; recall = .5998
+# For  val  dataset: loss = .5634; precision = .8714; recall = .6970
+history <- model |> fit(x = train_data |>  select(-class), y = train_data$class, 
+    epochs = 100, batch_size = 128, 
+    validation_data = list(val_data |>  select(-class), val_data$class),
+    callbacks = list(lr, stopping), verbose = 2)
 
-# Now is time to make the predictions with the new data to_predict. When I do
-# the predictions, I eliminate the variables class and price. Then I use the
-# function k_argmax() to convert the values to classes. And it's stored in the
-# object prediction
-prediction <-  predict(model, to_predict |> select(-c(class, price))) |> k_argmax()
+# Now inspect in test dataset, if the metrics can be similar:
+# For test dataset: loss = .6049; precision = .8739; recall = .6914
+model |> evaluate(test_data |> select(-class), test_data$class)
 
-# Now is time to evaluate the predictions, but first I need to apply data-wrangling.  
-# The object prediction is a vector and the first thing is convert it to a matrix
-# Then the matrix is converted to a tibble. Later the object is merged with the 
-# object to_predict using cbind(), then relocate some variables and then is
-# convert it to a tibble-object using as_tibble(). Then is stored in df object  
-df <- matrix(prediction, ncol = 1, dimnames = list(NULL, 'class_predicted')) |> 
-    as_tibble() |> cbind(to_predict) |> 
-    relocate(price, class, class_predicted, .after = last_col()) |> as_tibble()
-
-# Now it is time for evaluations. The dataset df have 3000 rows, also the same
-# dataframe have the variables CLASS and CLASS_PREDICTED. So, I want to know
-# how accurate were the predictions and for this I use filter() to compare the
-# values that are EQUAL in the variables CLASS and CLASS_PREDICTED. Then use
-# count() to count the number of observations. This code return 2451. So,
-# 2481 / 3000 = .827 This means that this model that will try to predict between
-# the 15 classes of the price of the diamonds, have an ACCURACY of 82.7%  
-df |> filter(class == class_predicted) |> count()
-
-# Now, I want to calculate the opposite, the procedure is very similar as the 
-# above, but this time I'm going to count all the number of observations that
-# are NOT EQUAL between CLASS and CLASS_PREDICTED. This code return 549. So,
-# 519 / 3000 = .173 This means that this model that will try to predict between
-# the 15 classes of the price of the diamonds, have an INACCURACY of 17.3%  
-df |> filter(class != class_predicted) |> count()
 
 
 
