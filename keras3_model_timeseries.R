25abf5e234ea6a9f4cc7c97c13d72c8321a3bce7
garzac <54555804+garzac@users.noreply.github.com>
2026-09-12 17:12:17 -0500

    Update keras3_model_timeseries.R

--- keras3_model_timeseries.R
+++ keras3_model_timeseries.R
@@ -1,11 +1,11 @@
 
 # The following network is a time-series model. It uses the dataset sunspots 
-# from datasets-package. Also uses the libraries tensorflow, keras and tidyverse.
+# from datasets-package. Also uses the libraries keras3, Tensorflow and Tidyverse.
 # The dataset-sunspots is a time-series and the values in data
 # are the number of spots in the sun. The values in the dataset are from 
 # January-1749 to December-1983. The main-objective of this neural-network
-# is that the model can be learn patterns in the data and predict the sunspots
-# for the month of January-1984
+# is if this model can detect and learn patterns in the data and predict the
+# sunspots for the month of January-1984
 
 # Loading the libraries
 library(keras3)
@@ -17,20 +17,18 @@ clear_session(free_memory = TRUE)
 
 # Now set up the random number for all the environment
 set_random_seed(51L)
-tf$random$set_seed(51L)
 
-# First is convert the time-series dataset sunspots to an array using 
-# the function array() and check the length of the array
-series <- array(sunspots)
+# First is convert the time-series dataset sunspots to an array using the function
+# array() and also normalizing the values by 10. Then verify the length of the array
+series <- array(sunspots) / 10
 length(series)
 
 # Now, define the hyper-parameters to transform the dataset
-split_time <- 1800
 window_size <- 50
 
 # Split the dataset into train/validation and check the length from both
-train_x <- series[1:split_time]
-val_x <- series[split_time:2820]
+train_x <- series[1:2000]
+val_x <- series[2001:2820]
 length(train_x)
 length(val_x)
 
@@ -40,13 +38,15 @@ length(val_x)
 # value from index 51 and defines it as the label. The next group starts from
 # indexes 2 to 51 and the label is the index 52. And so on ...
 # This process it is done with the train and validation datasets
-train <- timeseries_dataset_from_array(data = train_x,  
-    targets = tail(train_x, -50), sequence_length = window_size, batch_size = 128, 
-    shuffle = TRUE)
+train <- timeseries_dataset_from_array(data = train_x, 
+    targets = tail(train_x, - window_size), sequence_length = window_size, 
+    batch_size = 128)
 
-val <- timeseries_dataset_from_array(data = val_x, targets = tail(val_x, -50), 
-    sequence_length = window_size, batch_size = 128, shuffle = TRUE )
+val <- timeseries_dataset_from_array(data = val_x, targets = tail(val_x, - window_size), 
+    sequence_length = window_size, batch_size = 128)
 
+# We can inspect the first batch of data
+as_iterator(train) |> iter_next()
 
 # It's time to create the respective inputs and ouputs to build the model. 
  inputs <- keras_input(shape = c(window_size, 1))
@@ -60,7 +60,7 @@ outputs <- inputs |> layer_batch_normalization() |>
 # Now create the model
 model <- keras_model(inputs, outputs)
 
-# Inpsect the model's architecture
+# Inspect the model's architecture
 summary(model)
 
 # Now  compile the model with its optimizer and loss. For the loss I choose to 
@@ -68,27 +68,29 @@ summary(model)
 model |> compile(optimizer = optimizer_sgd(), loss = 'mae')
 
 # It's time for training. When the training finished. The metrics are:
-# Train dataset the loss = 11.3468; fot val dataset the loss = 17.0574
-model |> fit(train, epochs = 100, validation_data = val, verbose = 2)
+# In train dataset the loss = 1.6905; for val dataset the loss = 2.1574
+model |> fit(train, epochs = 196, validation_data = val, verbose = 2)
 
 # Next I'm going to extract the last 50 values from the series-object as the 
-# last sliding window. Then convert the series to an object np.array with the 
+# last sliding window. Then convert the series to an np.array object with the 
 # respective function.
-pred <- series[2771:2820]
-pred <- reticulate::np_array(pred)
+to_predict <- series[2771:2820]
+to_predict <- reticulate::np_array(to_predict)
 
 # Now I want to make a prediction that will predicts the sunspots for the entire
-# month of January-1984. Fot this Iuse the function tf$newaxis. The prediction
-# was 56.34731
-model |> predict(pred [tf$newaxis])
+# month of January-1984. The prediction is 5.76476. I normalized by 10 so the 
+# value predicted of sunpots for the entire month is 57.6476
+model |> predict(to_predict [tf$newaxis])
 
 # The real-value of sunspots for January-1984 is: 57.0 
-# The model predicted: 56.34731 For me is amazing how the model learned patterns            
-# and gave a really close value to the real value.
+# The model predicted: 57.6476. So the model was capable to detect and learn the 
+# patterns in the timeseries.
 
-# To check the real-value we can see it in the dataset sunspot.month 
+# To check the real-value we can see it in the dataset sunspot.m2014 
 # (its a bigger dataset than sunspots). To can see this dataset need to change 
 # some values in R with options() function and the parameter max.print
 options(max.print = 3100)
-sunspot.month
+sunspot.m2014
+
+
 
